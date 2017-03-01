0b8cff11855b98651ec782e36ff44ecee457a775
Robert <rgorbul@gmail.com>
2017-03-01 21:53:08 +0300

    Fix streaming events(RSS, Streamers)

--- Modules/StreamChecker.js
+++ Modules/StreamChecker.js
@@ -3,12 +3,15 @@ const isStreaming = require("./StreamerUtils.js");
 // Checks if a user is streaming on Twitch, YouTube Gaming, or HitBox and posts message in server channel if necessary
 module.exports = (winston, svr, serverDocument, streamerDocument, callback) => {
 	isStreaming(streamerDocument.type, streamerDocument._id, data => {
-		const updated = false;
+		let updated = false;
 
 		// Send status message if stream started
 		if(data && !streamerDocument.live_state) {
 			winston.info(`Streamer '${streamerDocument._id}' started streaming`, {svrid: svr.id});
-			const ch = streamerDocument.channel_id ? svr.channels.get(streamerDocument.channel_id) : svr.defaultChannel;
+      streamerDocument.live_state = true;
+      updated = true;
+
+      const ch = streamerDocument.channel_id ? svr.channels.get(streamerDocument.channel_id) : svr.defaultChannel;
 			if(ch) {
 				const channelDocument = serverDocument.channels.id(ch.id);
 				if(!channelDocument || channelDocument.bot_enabled) {
@@ -18,11 +21,12 @@ module.exports = (winston, svr, serverDocument, streamerDocument, callback) => {
 		// Update live_state if stream ended
 		} else if(!data && streamerDocument.live_state) {
 			streamerDocument.live_state = false;
+      updated = true;
 		}
 
-		// Save streamerDocument if necessary
+		// Save serverDocument if necessary
 		if(updated) {
-			streamerDocument.save(err => {
+			serverDocument.save(err => {
 				if(err) {
 					winston.error(`Failed to save data for streamer '${streamerDocument._id}'`, {svrid: svr.id}, err);
 				}
