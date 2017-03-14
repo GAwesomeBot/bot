3435b95a7b10f2b46dd9c2739c027fb2c08b3432
Frangu Vlad <kingdgrizzle@gmail.com>
2017-03-14 22:17:56 +0200

    Improved shardInfo messages.
    They will now appear in the READY event, which, while it might make the bot look like its not loading, you'll see the messages decently fast

--- Events/shardReady.js
+++
@@ -1,4 +0,0 @@
-// A shard receives the ready packet
-module.exports = (bot, db, config, winston, id) => {
-	winston.info(`Shard ${id + 1}/${config.shard_count || 1} connected: ${bot.guilds.size} guilds`);
-};
---
+++ Events/shardsReady.js
@@ -0,0 +1,14 @@
+// A shard receives the ready packet
+module.exports = (bot, db, config, winston) => {
+	winston.info(`Shards: ${config.shard_count}`);
+	let users = new Array(bot.shards.size).fill(0), guilds = new Array(bot.shards.size).fill(0);
+	bot.guilds.forEach(guild => {
+		users[guild.shard.id] += guild.members.size;
+		guilds[guild.shard.id] += 1;
+	});
+	bot.shards.forEach(shard => {
+		winston.info(`Shard ${shard.id + 1}/${config.shard_count || 1} connected: ${guilds[shard.id]} guilds, ${users[shard.id]} users`);
+	});
+
+	winston.info("All shards connected");
+};
