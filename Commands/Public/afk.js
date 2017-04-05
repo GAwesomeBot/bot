e001aa90576d096fe65f4363ccaf3f86ee3f2bfc
Robert <rgorbul@gmail.com>
2017-04-05 21:55:05 +0300

    Fix for public AFK command

--- Commands/Public/afk.js
+++ Commands/Public/afk.js
@@ -1,57 +1,62 @@
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix, commandData) => {
 	if(suffix) {
 		if(suffix == ".") {
-            userDocument.afk_message = null;
-            msg.channel.createMessage({
-                embed: {
-                    author: {
-                        name: bot.user.username,
-                        icon_url: bot.user.avatarURL,
-                        url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                    },
-                    color: 0x9ECDF2,
-                    description: "Welcome back! 🎊\nI removed your AFK message"
-                }
-            });
+			userDocument.afk_message = null;
+			msg.channel.createMessage({
+				embed: {
+					author: {
+						name: bot.user.username,
+						icon_url: bot.user.avatarURL,
+						url: "https://github.com/GilbertGobbels/GAwesomeBot"
+					},
+					color: 0x9ECDF2,
+					description: "Welcome back! 🎊\nI removed your AFK message"
+				}
+			});
 		} else {
-            userDocument.afk_message = suffix;
-            msg.channel.createMessage({
-                embed: {
-                    author: {
-                        name: bot.user.username,
-                        icon_url: bot.user.avatarURL,
-                        url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                    },
-                    color: 0x9ECDF2,
-                    description: `Alright, I'll show that when someone mentions you on this server. 👌\nUse \`${bot.getCommandPrefix(msg.channel.guild, serverDocument)}${commandData.name} .\` to remove it`
-                }
-            });
+			userDocument.afk_message = suffix;
+			msg.channel.createMessage({
+				embed: {
+					author: {
+						name: bot.user.username,
+						icon_url: bot.user.avatarURL,
+						url: "https://github.com/GilbertGobbels/GAwesomeBot"
+					},
+					color: 0x9ECDF2,
+					description: `Alright, I'll show that when someone mentions you on this server. 👌\nUse \`${bot.getCommandPrefix(msg.channel.guild, serverDocument)}${commandData.name} .\` to remove it`
+				}
+			});
 		}
+		userDocument.save(err => {
+			if(err) {
+				winston.error("Failed to save user data for AFK message", {usrid: msg.author.id}, err);
+			}
+		});
 	} else {
-		if(memberDocument.afk_message) {
-            msg.channel.createMessage({
-                embed: {
-                    author: {
-                        name: bot.user.username,
-                        icon_url: bot.user.avatarURL,
-                        url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                    },
-                    color: 0x9ECDF2,
-                    description: `You have the AFK message \`${memberDocument.afk_message}\` set on this server 💭`
-                }
-            });
+		if(userDocument.afk_message) {
+			msg.channel.createMessage({
+				embed: {
+					author: {
+						name: bot.user.username,
+						icon_url: bot.user.avatarURL,
+						url: "https://github.com/GilbertGobbels/GAwesomeBot"
+					},
+					color: 0x9ECDF2,
+					description: `You have the AFK message \`${userDocument.afk_message}\` set on this server 💭`
+				}
+			});
 		} else {
-            msg.channel.createMessage({
-                embed: {
-                    author: {
-                        name: bot.user.username,
-                        icon_url: bot.user.avatarURL,
-                        url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                    },
-                    color: 0x00FF00,
-                    description: "You don't have an AFK message set on this server ⌨"
-                }
-            });
+			msg.channel.createMessage({
+				embed: {
+					author: {
+						name: bot.user.username,
+						icon_url: bot.user.avatarURL,
+						url: "https://github.com/GilbertGobbels/GAwesomeBot"
+					},
+					color: 0x00FF00,
+					description: "You don't have an AFK message set on this server ⌨"
+				}
+			});
 		}
 	}
 };
