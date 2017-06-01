6ece5d25900ff1372f870c2c22f8c9088383c018
Hilbert Gilbertson <HilbertGilbertson@ctos.online>
2017-06-01 18:22:45 +0100

    Ban now checks user role hierarchy
    The Bot Admin's highest role on the guild must exceed the highest
    role of the target user (unless the Admin is the guild owner).

--- Commands/Public/ban.js
+++ Commands/Public/ban.js
@@ -1,6 +1,35 @@
 const ModLog = require("./../../Modules/ModerationLogging.js");
 
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix) => {
+	const hierarchy = member => {
+		if (msg.channel.guild.ownerID == msg.author.id) {
+			return true;
+		}
+		let admin = 0;
+		msg.member.roles.forEach(roleID => {
+			let urole = msg.channel.guild.roles.get(roleID);
+			if (urole) {
+				if (urole.position > admin) {
+					admin = urole.position;
+				}
+			}
+		});
+		let target = 0;
+		member.roles.forEach(roleID => {
+			let urole = msg.channel.guild.roles.get(roleID);
+			if (urole) {
+				if (urole.position > target) {
+					target = urole.position;
+				}
+			}
+		});
+		if (admin > target) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	if(suffix) {
 		let member, reason;
 		if(suffix.indexOf("|" )> -1 && suffix.length > 3) {
@@ -10,36 +39,41 @@ module.exports = (bot, db, config, winston, userDocument, serverDocument, channe
 			member = bot.memberSearch(suffix, msg.channel.guild);
 		}
 		if(member) {
-			member.ban(1).then(() => {
-				msg.channel.createMessage({
-					embed: {
-                        author: {
-                            name: bot.user.username,
-                            icon_url: bot.user.avatarURL,
-                            url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                        },
-                        color: 0x00FF00,
-						title: `Bye-bye **@${bot.getName(msg.channel.guild, serverDocument, member)}** 🔨`,
-						image: {
-							url: "http://media-kingdgrizzle.tk/2017/01/62ac6016dc438a1fe4926ed43d3fe280.gif"
+			const hascontrol = hierarchy(member);
+			if (hascontrol){
+				member.ban(1).then(() => {
+					msg.channel.createMessage({
+						embed: {
+							author: {
+								name: bot.user.username,
+                icon_url: bot.user.avatarURL,
+                url: "https://github.com/GilbertGobbels/GAwesomeBot"
+							},
+							color: 0x00FF00,
+							title: `Bye-bye **@${bot.getName(msg.channel.guild, serverDocument, member)}** 🔨`,
+							image: {
+								url: "http://media-kingdgrizzle.tk/2017/01/62ac6016dc438a1fe4926ed43d3fe280.gif"
+							}
 						}
-					}
+					});
+					ModLog.create(msg.channel.guild, serverDocument, "Ban", member, msg.member, reason);
+				}).catch(err => {
+					winston.error(`Failed to ban member '${member.user.username}' from server '${msg.channel.guild.name}'`, {svrid: msg.channel.guild.name, usrid: member.id}, err);
+					msg.channel.createMessage({
+						embed: {
+							author: {
+								name: bot.user.username,
+                icon_url: bot.user.avatarURL,
+                url: "https://github.com/GilbertGobbels/GAwesomeBot"
+							},
+							color: 0xFF0000,
+							description: `I couldn't ban **@${bot.getName(msg.channel.guild, serverDocument, member)}** 🍇`
+						}
+					});
 				});
-				ModLog.create(msg.channel.guild, serverDocument, "Ban", member, msg.member, reason);
-			}).catch(err => {
-				winston.error(`Failed to ban member '${member.user.username}' from server '${msg.channel.guild.name}'`, {svrid: msg.channel.guild.name, usrid: member.id}, err);
-				msg.channel.createMessage({
-					embed: {
-                        author: {
-                            name: bot.user.username,
-                            icon_url: bot.user.avatarURL,
-                            url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                        },
-                        color: 0xFF0000,
-						description: `I couldn't ban **@${bot.getName(msg.channel.guild, serverDocument, member)}** 🍇`
-					}
-				});
-			});
+			} else {
+				msg.channel.createMessage(`${msg.author.mention} You don't have permission to manage this user.`);
+			}
 		} else {
 			msg.channel.createMessage({
 				embed: {
