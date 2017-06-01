d61232cb98a05a99da3a86f29b87931c8f34a116
Frangu Vlad <kingdgrizzle@gmail.com>
2017-06-02 01:36:44 +0300

    OOppss :P

--- Modules/HastebinUpload.js
+++ Modules/HastebinUpload.js
@@ -5,12 +5,33 @@ const unirest = require("unirest");
  * of the callback
  */
 /* eslint-disable indent */
-module.exports = (query, callback) => {
-  unirest.post("https://hastebin.com/documents").send(query).end(res => {
-    if (res.status === 200 && res.body.key) {
-      return callback(`https://hastebin.com/${res.body.key}`, `https://hastebin.com/raw/${res.body.key}`);
+module.exports = (type, query) => {
+  if (!query) {
+    query = type;
+    type = "n";
+  }
+  return new Promise((resolve, reject) => {
+    if (["r", "raw", "n", "normal"].includes(type.toLowerCase())) {
+      unirest.post("https://hastebin.com/documents").send(query).end(res => {
+        if (res.status === 200 && res.body.key) {
+          switch (type.toLowerCase()) {
+            case "r":
+            case "raw":
+              resolve(`https://hastebin.com/raw/${res.body.key}`);
+              break;
+            case "n":
+            case "normal":
+              resolve(`https://hastebin.com/${res.body.key}`);
+              break;
+            default:
+              resolve(`https://hastebin.com/${res.body.key}`);
+          }
+        } else {
+          reject(Error("Couldn't upload to hastebin, try again later!"));
+        }
+      });
     } else {
-      return callback(`https://hastebin.com/`);
+      reject(TypeError(`Type must be either "r", "raw", "n" or "normal" (case insensitive). Received ${type} instead.`));
     }
   });
 };
