2f67a6825b336dba2ff591c3416b2ee440c1ee36
Frangu Vlad <kingdgrizzle@gmail.com>
2017-06-01 23:25:01 +0300

    ~Are you happy now hilbert?~ Updated a few commands to have the new embeds, and / or new features.
    - debug now takes an optional `-v` argument to show process information
    
    - e621 now has a nsfw channel check! Also, a cool embed!
    
    - ban now asks for a confirmation before  banning. Cause safety is
    number one priority.
    
    - gif now shows the gosh darn gif!
    
    - google is also embedded, and looking smexy
    
    - 9/10 dogfact was already a thing
    
    - convert now supports money! Woosh! Warning: you need to provide an api
    key
    
    - anime is really slick now.

--- Commands/Public/convert.js
+++ Commands/Public/convert.js
@@ -1,66 +1,72 @@
 const convert = require("convert-units");
-const fx = require("money");
+const Currency = require("./../../Modules/ConvertCurrency.js");
+const TimeTaken = require("./../../Modules/TimeTaken.js");
 
-module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix, commandData) => {
+/* eslint-disable max-len */
+module.exports = async (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix, commandData) => {
 	const args = suffix.split(" ");
-	if(args.length == 4 && args[2].toLowerCase() == "to") {
+	if (args.length === 4 && args[2].toLowerCase() === "to") {
 		args.splice(2, 1);
 	}
-	if(args.length == 3 && args[0] && !isNaN(args[0]) && args[1] && args[2]) {
+	if (args.length === 3 && args[0] && !isNaN(args[0]) && args[1] && args[2]) {
+		let m = await msg.channel.createMessage({
+			embed: {
+				color: 0x9ECDF2,
+				description: `Converting...`,
+				footer: {
+					text: `This shouldn't take long...`,
+				},
+			},
+		});
 		try {
-			msg.channel.createMessage({
+			const converted = Math.round(convert(args[0]).from(args[1]).to(args[2]) * 1000) / 1000;
+			const timeTaken = TimeTaken(m, msg);
+			m.edit({
 				embed: {
-                    author: {
-                        name: bot.user.username,
-                        icon_url: bot.user.avatarURL,
-                        url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                    },
-                    color: 0x00FF00,
-					title: "Conversion Result:",
-					description: `${Math.round(convert(args[0]).from(args[1]).to(args[2]) * 1000) / 1000} ${args[2]}`
-				}
+					color: 0x00FF00,
+					description: `\`\`\`${converted}${args[2]}\`\`\``,
+					footer: {
+						text: `It took ${timeTaken}ms to convert.`,
+					},
+				},
 			});
-		} catch(err) {
+		} catch (err) {
 			try {
-				msg.channel.createMessage({
-					embed: {
-                        author: {
-                            name: bot.user.username,
-                            icon_url: bot.user.avatarURL,
-                            url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                        },
-                        color: 0x00FF00,
-						title: "Conversion Result",
-						description: `${(Math.round(fx(parseFloat(args[0])).from(args[1].toUpperCase()).to(args[2].toUpperCase())) * 100) / 100} ${args[2].toUpperCase()}`
-					}
+				let converted;
+				Currency.convert(args[0], args[1], args[2]).then(res => {
+					converted = res.amount;
 				});
-			} catch(err) {
-				winston.warn(`Unsupported conversion units '${args[1]}' and '${args[2]}' provided for ${commandData.name} command`, {svrid: msg.channel.guild.id, chid: msg.channel.id, usrid: msg.author.id});
+				const timeTaken = TimeTaken(m, msg);
+				m.edit({
+					embed: {
+						color: 0x00FF00,
+						description: `\`\`\`${converted}${args[2].toUpperCase()}\`\`\``,
+						footer: {
+							text: `It took ${timeTaken}ms to convert.`,
+						},
+					},
+				});
+				// eslint-disable-next-line
+			} catch (err) {
+				winston.warn(`Unsupported conversion units "${args[1]}" and "${args[2]}" provided for "${commandData.name}" command`, { svrid: msg.channel.guild.id, chid: msg.channel.id, usrid: msg.author.id });
 				msg.channel.createMessage({
 					embed: {
-                        author: {
-                            name: bot.user.username,
-                            icon_url: bot.user.avatarURL,
-                            url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                        },
-                        color: 0xFF0000,
-						description: `I don't support that unit, please try something else ↩. You can use standard currency codes or any of [these](https://github.com/ben-ng/convert-units#supported-units)`
-					}
+						color: 0xFF0000,
+						description: `I don't support that unit, please try something else ↩.\nYou can use standard currency codes or any of [these](https://github.com/ben-ng/convert-units#supported-units)`,
+					},
 				});
 			}
 		}
 	} else {
-		winston.warn(`Invalid parameters '${suffix}' provided for ${commandData.name} command`, {svrid: msg.channel.guild.id, chid: msg.channel.id, usrid: msg.author.id});
+		winston.warn(`Invalid parameters '${suffix}' provided for ${commandData.name} command`, { svrid: msg.channel.guild.id, chid: msg.channel.id, usrid: msg.author.id });
 		msg.channel.createMessage({
 			embed: {
-                author: {
-                    name: bot.user.username,
-                    icon_url: bot.user.avatarURL,
-                    url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                },
-                color: 0xFF0000,
-				description: `Huh? Make sure to use the syntax \`${bot.getCommandPrefix(msg.channel.guild, serverDocument)}${commandData.name} ${commandData.usage}\``
-			}
+				color: 0xFF0000,
+				description: `Huh? Did you mistype?`,
+				footer: {
+					text: `Make sure to use the syntax "${bot.getCommandPrefix(msg.channel.guild, serverDocument)}${commandData.name}${commandData.usage}"`,
+				},
+			},
 		});
 	}
 };
--- Commands/Public/emotes.js
+++ Commands/Public/emotes.js
@@ -1,30 +1,21 @@
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg) => {
-	if(msg.channel.guild.emojis.length > 0) {
+	const emojis = msg.channel.guild.emojis.map(emoji => { //eslint-disable-line
+		return `<:${emoji.name}:${emoji.id}>: ${emoji.name}`;
+	});
+	if (msg.channel.guild.emojis.length > 0) {
 		msg.channel.createMessage({
 			embed: {
-                author: {
-                    name: bot.user.username,
-                    icon_url: bot.user.avatarURL,
-                    url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                },
-                color: 0x00FF00,
-				title: `**${msg.channel.guild.emojis.length} custom emote${msg.channel.guild.emojis.length == 1 ? "" : "s"} on this server:**`,
-				description: msg.channel.guild.emojis.map(emoji => {
-								return `<:${emoji.name}:${emoji.id}>: ${emoji.name}`;
-							}).join(`\n`)
-			}
+				color: 0x00FF00,
+				title: `**${msg.channel.guild.emojis.length} custom emote${msg.channel.guild.emojis.length === 1 ? "" : "s"} on this server:**`,
+				description: emojis.join("\n"),
+			},
 		});
 	} else {
 		msg.channel.createMessage({
 			embed: {
-                author: {
-                    name: bot.user.username,
-                    icon_url: bot.user.avatarURL,
-                    url: "https://github.com/GilbertGobbels/GAwesomeBot"
-                },
-                color: 0xFF0000,
-				description: "There aren't any custom emotes on this server. 🌋"
-			}
+				color: 0xFF0000,
+				description: "There aren't any custom emotes on this server. 🌋",
+			},
 		});
 	}
 };
