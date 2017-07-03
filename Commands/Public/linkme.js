99c9c3ffd4febac72b87a28d260001e56dd547cb
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-03 19:17:50 +0300

    Now with nice embeds :D

--- Commands/Public/linkme.js
+++ Commands/Public/linkme.js
@@ -3,8 +3,8 @@ const gplay = require("google-play-scraper");
 const getAppList = suffix => {
 	const apps = suffix.split(",");
 	let i = 0;
-	while(i < apps.length) {
-		if(!apps[i] || apps.indexOf(apps[i]) != i) {
+	while (i < apps.length) {
+		if (!apps[i] || apps.indexOf(apps[i]) !== i) {
 			apps.splice(i, 1);
 		} else {
 			apps[i] = apps[i].trim();
@@ -14,39 +14,62 @@ const getAppList = suffix => {
 	return apps;
 };
 
+/* eslint-disable max-len */
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix) => {
 	const apps = getAppList(suffix);
-	if(apps.length > 0) {
-		const results = [];
-		const fetchApp = (i, callback) => {
-			if(i >= apps.length) {
-				callback();
+	if (apps.length > 0) {
+		let results = [];
+		const fetchApp = (i, cb) => {
+			if (i >= apps.length) {
+				return cb();
 			} else {
 				gplay.search({
 					term: apps[i],
-					num: 1
+					num: 1,
 				}).then((data, err) => {
-					if(!err && data && data.length > 0) {
-						let info = `**${data[0].title}** by ${data[0].developer}, `;
-						if(data[0].free) {
-							info += "free";
-						} else {
-							info += data[0].price;
-						}
-						info += ` and rated ${data[0].score} stars: <${data[0].url}>`;
-						results.push(info);
+					if (!err && data && data.length > 0) {
+						results.push({
+							color: 0x00FF00,
+							author: {
+								name: `By ${data[0].developer}`,
+							},
+							thumbnail: {
+								url: data[0].icon ? `http:${data[0].icon}` : "",
+							},
+							title: `__${data[0].title}__`,
+							url: `${data[0].url}`,
+							description: `A quick summary: \`\`\`\n${data[0].summary}\`\`\``,
+							footer: {
+								text: `Rated ${data[0].score} ⭐ | ${data[0].free ? `This app is free` : `This app costs ${data[0].price}`}`,
+							},
+						});
 					} else {
-						winston.warn(`Google Play app '${apps[i]}' not found to link`, {svrid: msg.channel.guild.id, chid: msg.channel.id, usrid: msg.author.id});
-						results.push(`❌ No results found for \`${apps[i]}\``);
+						winston.warn(`Google Play app "${apps[i]}" not found to link`, { svrid: msg.channel.guild.id, chid: msg.channel.id, usrid: msg.author.id });
+						results.push({
+							color: 0xFF0000,
+							description: `There were no results for the app called \`${apps[i]}\`.❌`,
+							footer: {
+								text: `You should try searching for the app again, making sure you spelt it right.`,
+							},
+						});
 					}
-					fetchApp(++i, callback);
+					fetchApp(++i, cb);
 				});
 			}
 		};
 		fetchApp(0, () => {
-			bot.sendArray(msg.channel, results);
+			for (let i = 0; i < results.length; i++) {
+				msg.channel.createMessage({
+					embed: results[i],
+				});
+			}
 		});
 	} else {
-		msg.channel.createMessage("https://play.google.com/store/apps/");
+		msg.channel.createMessage({
+			embed: {
+				color: 0xFF0000,
+				description: `[What app would you like to find today..? 🤔](https://play.google.com/store/apps/)`,
+			},
+		});
 	}
 };
