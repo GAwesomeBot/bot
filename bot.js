3f84761b7f5a69191ab68584bac363cf64e37fcc
GilbertGobbels <mulie4@live.nl>
2017-07-28 12:20:15 +0200

    Better wording

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const configJSON 	= require("./Configurations/config.json");
 
 /*
  * The following vars shouldn't be used:
- * wanston, its used for initializing the global winston variable. Use that instead
+ * wanston, used for initializing the global winston variable. Use that instead
  * chalk, moment, winston-rotate-files are only used in the logger creation process as well
  * So you should require them whenever needed.
  * These variables are more for the logger setup than for general usage
@@ -29,6 +29,7 @@ global.winston = new wanston.Logger({
 			timestamp: () => `[${chalk.grey(moment().format("HH:mm:ss"))}]`,
 		}),
 		new wanston.transports.DailyRotateFile({
+            level: "silly",
 			colorize: false,
 			datePattern: `dd-MM-yyyy.`,
 			prepend: true,
@@ -45,20 +46,20 @@ global.winston = new wanston.Logger({
 		}),
 	],
 });
-winston.info("Logger has been loaded.");
+winston.info(`Logging to ${require("path").join(process.cwd(), 'logs/gawesomebot.log')}.`);
 
 let db;
 (async () => {
 	try {
 		await database.initialize(configJS.databaseURL);
 	} catch (err) {
-		winston.error(`There has been a database error! Is the database launched?\n`, err);
+		winston.error(`An error occurred while connecting to MongoDB! Is the database online?\n`, err);
 		process.exit(-1);
 	}
 })();
 db = database.getConnection();
 if (db) {
-	winston.info(`Connected to the database successfuly.`);
+	winston.info(`Connected to the database successfully.`);
 	const bot = require("./Discord.js")(db, configJS, configJSON);
 	bot.login(auth.discord.clientToken).then(token => {
 		winston.info("Started Bot Application", { token: token });
