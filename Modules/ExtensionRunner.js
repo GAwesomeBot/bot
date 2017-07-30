b100fa028d174d1dd2defb8f0a820035c6ee50e1
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-30 15:16:25 +0300

    Incomplete extension runner

---
+++ Modules/ExtensionRunner.js
@@ -0,0 +1,30 @@
+ // TODO: getSandbox
+
+const domain = require("domain");
+const fs = require("fs-extra");
+const vm = require("vm");
+
+// Run an extension (command, keyword, or timer) in the sandbox
+/* eslint-disable max-len */
+module.exports = async (bot, db, server, serverDocument, channel, extensionDocument, msg, suffix, keywordMatch) => {
+	// TODO: extensionSandbox = new vm.createContext(getSandbox)
+	let extensionCode;
+	try {
+		extensionCode = await fs.readFile(`${__dirname}/../Extensions/${server.id}-${extensionDocument._id}.gabext`, "utf8");
+	} catch (err) {
+		winston.warn(`Failed to load the extension code for ${extensionDocument.type} extension "${extensionDocument.name}"`, { svrid: server.id, extid: extensionDocument._id }, err);
+	}
+	if (extensionCode) {
+		const extensionDomain = domain.create();
+		extensionDomain.run(() => {
+			const extensionVM = new vm.Script(`(async () => {${extensionCode}})()`);
+			// extensionVM.runInContext(extensionSandbox, {
+			// 	displayErrors: true,
+			// 	timeout: 10000,
+			// });
+		});
+		extensionDomain.on("error", err => {
+			winston.warn(`Failed to run ${extensionDocument.type} extension "${extensionDocument.name}": ${err.stack}`, { svrid: server.id, chid: channel.id, extid: extensionDocument._id});
+		});
+	}
+};
