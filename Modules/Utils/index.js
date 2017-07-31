32951a893801a52f1c8a40bdf9737d997590700c
Frangu Vlad <kingdgrizzle@gmail.com>
2017-07-31 11:50:26 +0300

    Giveaways and Countdowns wooo!

--- Modules/Utils/index.js
+++ Modules/Utils/index.js
@@ -4,9 +4,11 @@ module.exports = {
 	ClearServerStats: require("./ClearServerStats.js"),
 	FilterChecker: require("./FilterChecker.js"),
 	Gist: require("./GitHubGist.js"),
+	Giveaways: require("./Giveaways.js"),
 	GlobalDefines: require("./GlobalDefines.js"),
 	ObjectDefines: require("./ObjectDefines.js"),
 	RankScoreCalculator: require("./RankScoreCalculator.js"),
 	RegExpMaker: require("./RegExpMaker.js"),
+	SetCountdown: require("./SetCountdown.js"),
 	SetReminder: require("./SetReminder.js"),
 };
