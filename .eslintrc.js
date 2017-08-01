f3e7592f81a95a12540f37e494947ba00e33a2ab
Frangu Vlad <contact@kingdgrizzle.ml>
2017-08-01 21:14:15 +0300

    Wrong setting for this eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -100,7 +100,7 @@ module.exports = {
 		"semi-spacing": "error",
 		"semi": "error",
 		"space-before-blocks": "error",
-		"space-before-function-paren": ["error", "never"],
+		"space-before-function-paren": ["error", "always"],
 		"space-in-parens": "error",
 		"space-infix-ops": "error",
 		"space-unary-ops": "error",
