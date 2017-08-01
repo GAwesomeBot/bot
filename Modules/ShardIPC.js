6c6d2b41fbbe703d5a62fb2cebfad6e1a939e117
Frangu Vlad <contact@kingdgrizzle.ml>
2017-08-01 16:22:13 +0300

    Eslint for Gilbert <3

--- Modules/ShardIPC.js
+++ Modules/ShardIPC.js
@@ -1,31 +1,32 @@
-const Discord = require("discord.js");
+// TODO: Uncomment if you are using it.
+// const Discord = require("discord.js");
 const EventEmitter = require("events");
 
 class SharderIPC extends EventEmitter {
 	constructor(sharder, winston) {
 		super();
-		this.sharder = sharder
-		this.winston = winston
+		this.sharder = sharder;
+		this.winston = winston;
 	}
-	
+
 	listen() {
-		this.winston.verbose("Launched sharder listener.")
+		this.winston.verbose("Launched sharder listener.");
 		this.sharder.on("message", (shard, msg) => {
-			this.winston.verbose("Recieved message from shard.", {msg: msg, shard: shard.id});
+			this.winston.verbose("Recieved message from shard.", { msg: msg, shard: shard.id });
 			try {
 				const payload = JSON.parse(msg);
 				this.emit(payload.subject, payload, shard.id);
 			} catch (err) {
-				this.winston.warn("Unable to handle message from shard :C\n", {msg: msg, shard: shard.id, err: err});
+				this.winston.warn("Unable to handle message from shard :C\n", { msg: msg, shard: shard.id, err: err });
 			}
-		})
+		});
 	}
-	
+
 	send(subject, payload, shard) {
 		try {
-			this.winston.verbose("Sending message to shard", {subject: subject, payload: payload, shard: shard});
+			this.winston.verbose("Sending message to shard", { subject: subject, payload: payload, shard: shard });
 			payload.subject = subject;
-			
+
 			if (shard === "*") {
 				this.sharder.broadcast(JSON.stringify(payload)).catch(err => {
 					throw err;
@@ -37,7 +38,7 @@ class SharderIPC extends EventEmitter {
 				});
 			}
 		} catch (err) {
-			this.winston.warn("Failed to send message to shard :C\n", {subject: subject, payload: payload, shard: shard, err: err});
+			this.winston.warn("Failed to send message to shard :C\n", { subject: subject, payload: payload, shard: shard, err: err });
 		}
 	}
 }
@@ -49,27 +50,27 @@ class ShardIPC extends EventEmitter {
 		this.winston = winston;
 		this.proc = proc;
 	}
-	
+
 	listen() {
-		this.winston.verbose("Launched shard listener.")
-		this.proc.on("message", (msg) => {
+		this.winston.verbose("Launched shard listener.");
+		this.proc.on("message", msg => {
 			try {
-				this.winston.verbose("Recieved message from sharder.", {msg: msg});	
+				this.winston.verbose("Recieved message from sharder.", { msg: msg });
 				const payload = JSON.parse(payload);
 				this.emit(payload.subject, payload);
 			} catch (err) {
-				this.winston.warn("Unable to handle message from master :C\n", {msg: msg, err: err});
+				this.winston.warn("Unable to handle message from master :C\n", { msg: msg, err: err });
 			}
-		})
+		});
 	}
-	
+
 	send(subject, payload) {
 		try {
-			this.winston.verbose("Sending message to master", {subject: subject, payload: payload});
+			this.winston.verbose("Sending message to master", { subject: subject, payload: payload });
 			payload.subject = subject;
 			this.shardClient.send(JSON.stringify(payload));
 		} catch (err) {
-			this.winston.warn("Failed to send message to master :C\n", {payload: payload, subject: subject, err: err});
+			this.winston.warn("Failed to send message to master :C\n", { payload: payload, subject: subject, err: err });
 		}
 	}
 }
