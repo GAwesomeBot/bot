c820a19cf20a8e6781550e2e17b72363d93efc6e
Frangu Vlad <contact@kingdgrizzle.ml>
2017-08-01 16:31:12 +0300

    Alphabetical Betty

--- Modules/index.js
+++ Modules/index.js
@@ -2,8 +2,8 @@ module.exports = {
 	Console: require("./Console.js"),
 	ConvertCurrency: require("./ConvertCurrency.js"),
 	ExtensionRunner: require("./ExtensionRunner.js"),
-	ShardIPC: require("./ShardIPC.js").ShardIPC,
 	SharderIPC: require("./ShardIPC.js").SharderIPC,
+	ShardIPC: require("./ShardIPC.js").ShardIPC,
 	Updater: require("./Updater.js"),
 	Utils: require("./Utils/"),
 };
