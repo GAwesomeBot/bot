5d0d47190131d924a8353ca2f1e19f229e54a5f6
Frangu Vlad <contact@kingdgrizzle.ml>
2017-09-17 15:17:01 +0300

    [Docs] Better BaseEvent docs

--- Events/BaseEvent.js
+++ Events/BaseEvent.js
@@ -15,7 +15,7 @@ class BaseEvent {
 
 	/**
 	 * Public handler for events
-	 * @param {*} args The args that the event emitted
+	 * @param {?Object} [values] The values that the event emitted
 	 */
 	async handle () {
 		throw new Error(`${this.constructor.name} doesn't have a handle method!`);
@@ -23,7 +23,7 @@ class BaseEvent {
 
 	/**
 	 * Call this function to handle events if the requirement is set
-	 * @param {*} args Params to hand over to the requirement check, and to the event
+	 * @param {?Object} [values={}] Params to hand over to the requirement check, and to the event
 	 * @private
 	 */
 	async _handle (values = {}) {
@@ -34,7 +34,7 @@ class BaseEvent {
 
 	/**
 	 * Simple logic for checking if the event should run or not.
-	 * @param {*} args The args object that the event emitted
+	 * @param {?Object} [values] The values object that the event emitted
 	 * @returns {Boolean}
 	 */
 	requirements () {
