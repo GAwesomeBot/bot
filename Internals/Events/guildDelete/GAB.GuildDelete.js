f033fcaba9169ff180b6d7230d4b66fc6e467b53
Gilbert Gobbels <gijsh55@gmail.com>
2017-12-02 13:03:44 +0100

    [GuildDelete] Finish GuildDelete event

--- Internals/Events/guildDelete/GAB.GuildDelete.js
+++ Internals/Events/guildDelete/GAB.GuildDelete.js
@@ -3,9 +3,8 @@ const { PostShardedData } = require("../../../Modules/");
 
 class GuildDelete extends BaseEvent {
 	async handle (guild) {
-		// TODO: Gilbert: the IPC guild thingie
-		// TODO: GILBERT DO THE IPC REMOVE GUILD THING DAMMIT
-		this.bot.IPC.send("sendAllGuilds", {});
+		this.bot.IPC.send("guilds", { remove: [guild.id] });
+		this.bot.IPC.send("sendAllGuilds");
 		await PostShardedData(this.bot);
 		winston.info(`Left server ${guild} :(`, { svrid: guild.id });
 	}
