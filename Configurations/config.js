370c6ffba8bd5cb6923f358e1ff301e53d565232
Gilbert Gobbels <gijsh55@gmail.com>
2017-12-06 18:19:49 +0100

    [Config] Some fixes hehe

--- Configurations/config.js
+++ Configurations/config.js
@@ -49,7 +49,7 @@ const configs = {
 	 * error, warn, info, debug and verbose.
 	 * Recommended to stay as info to prevent console cluttering.
 	 * fileLevel determines the output to master/shard.gawesomebot.log and verbose.gawesomebot.log files.
-	 * Note that setting these values to a level lower than `info` might influence your bot's performence.
+	 * Note that setting these values to a level lower than `info` might influence your bot's performance.
 	 */
 	consoleLevel: "info",
 	fileLevel: "info",
