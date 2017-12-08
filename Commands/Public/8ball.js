7cafb9bbc788cc653328c87f4e82cb8c6168af52
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-08 10:49:28 +0200

    [Commands/8ball] Finished 8ball

--- Commands/Public/8ball.js
+++ Commands/Public/8ball.js
@@ -1,10 +1,93 @@
 /**
  * 8ball!
  */
-const request = require("snekfetch");
-
-module.exports = async (main, documents, msg, suffix, commandData) => {
-	if (suffix) {
+module.exports = async (main, documents, msg, commandData) => {
+	const { Utils: { PromiseWait } } = main;
+	if (msg.suffix) {
+		// Deal with it™️
+		let randomWaitTimes = [1000, 1500, 3000, 2500, 2000, 1250, 500, 300, 100, 600];
+		let randomAnswers = [
+			{
+				color: 0x43B581,
+				answer: "It is certain",
+			},
+			{
+				color: 0x43B581,
+				answer: "It is decidedly so",
+			},
+			{
+				color: 0x43B581,
+				answer: "Without a doubt",
+			},
+			{
+				color: 0x43B581,
+				answer: "Yes, definitely",
+			},
+			{
+				color: 0x43B581,
+				answer: "You may rely on it",
+			},
+			{
+				color: 0x43B581,
+				answer: "As I see it, yes",
+			},
+			{
+				color: 0x43B581,
+				answer: "Most likely",
+			},
+			{
+				color: 0x43B581,
+				answer: "Outlook good",
+			},
+			{
+				color: 0x43B581,
+				answer: "Yes",
+			},
+			{
+				color: 0x43B581,
+				answer: "Signs point to yes",
+			},
+			{
+				color: 0xE55B0A,
+				answer: "Reply hazy try again",
+			},
+			{
+				color: 0xE55B0A,
+				answer: "Ask again later",
+			},
+			{
+				color: 0xE55B0A,
+				answer: "Better not tell you now",
+			},
+			{
+				color: 0xE55B0A,
+				answer: "Cannot predict now",
+			},
+			{
+				color: 0xE55B0A,
+				answer: "Concentrate and ask again",
+			},
+			{
+				color: 0xCC0F16,
+				answer: "Don't count on it",
+			},
+			{
+				color: 0xCC0F16,
+				answer: "My reply is no",
+			},
+			{
+				color: 0xCC0F16,
+				answer: "My sources say no",
+			},
+			{
+				color: 0xCC0F16,
+				answer: "Outlook not so good",
+			},
+			{
+				color: 0xCC0F16,
+				answer: "Very doubtful",
+			},
+		];
 		const m = await msg.channel.send({
 			embed: {
 				color: 0x3669FA,
@@ -14,30 +97,14 @@ module.exports = async (main, documents, msg, suffix, commandData) => {
 				},
 			},
 		});
-		const res = await request.get(`https://8ball.delegator.com/magic/JSON/${encodeURIComponent(suffix)}`).set("Accept", "application/json");
-		if (res.status === 200) {
-			m.edit({
-				embed: {
-					color: 0x00FF00,
-					title: `Our 🎱 replied with...`,
-					description: `\`\`\`css\n${res.body.magic.answer}\`\`\``,
-					footer: {
-						text: `This answers type is "${res.body.magic.type}". Interesting..`,
-					},
-				},
-			});
-		} else {
-			winston.verbose(`Failed to get 8ball answer, API returned "${res.status}"`, { svrid: msg.channel.guild.id, chid: msg.channel.id, statusText: res.statusText });
-			m.edit({
-				embed: {
-					color: 0xFF0000,
-					description: `Dang.. I think our 🎱 broke.. 😞`,
-					footer: {
-						text: `You can try again! Maybe our 8ball fell asleep..`,
-					},
-				},
-			});
-		}
+		await PromiseWait(randomWaitTimes[Math.floor(Math.random() * randomWaitTimes.length)]);
+		let randomChoice = randomAnswers[Math.floor(Math.random() * randomAnswers.length)];
+		m.edit({
+			embed: {
+				description: `Our 🎱 replied with:\`\`\`css\n${randomChoice.answer}\`\`\``,
+				color: randomChoice.color,
+			},
+		});
 	} else {
 		winston.verbose(`No suffix was provided for the "${commandData.name}" command`, { svrid: msg.guild.id, chid: msg.channel.id, usrid: msg.author.id	});
 		msg.channel.send({
