ad37e3aa5507ea3df27b38a3cd07ec82d09fdeb5
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-10 11:41:23 +0200

    [PMCommands/config] Finish config command

---
+++ Commands/PM/config.js
@@ -0,0 +1,9 @@
+module.exports = async ({ configJS }, msg, commandData) => {
+	msg.reply({
+		embed: {
+			color: 0xFF0000,
+			title: `This command is deprecated!`,
+			description: `You can visit the dashboard by going [here](${configJS.hostingURL}/dashboard)`,
+		},
+	});
+};
