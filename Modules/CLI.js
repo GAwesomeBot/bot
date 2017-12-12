0644f4f0ab0f280b7fe08a500b026fe91ca5723a
TTtie <tttiett@outlook.com>
2017-12-12 19:04:02 +0100

    [CLI] Add the stringified constant; we're not done yet!

--- Modules/CLI.js
+++ Modules/CLI.js
@@ -5,17 +5,30 @@ class CLI {
 		this.stdin = process.stdin;
 	}
 
+	/*
+	* Sets up all the stuff we need
+	* @returns {CLI}
+	*/
 	setup () {
 		this.stdin.on("data", this.listener.bind(this));
+		return this;
 	}
 
+	/*
+	* Disposes (destroys) all the CLI things.
+	* @returns {CLI]*/
 	dispose () {
 		this.stdin.removeListener("data", this.listener.bind(this));
 	}
 
+	/*
+	* Listens to the incoming events from stdin
+	* @param {Buffer} data The incoming data from the stdin*/
+	// No WebStorm, it can't be static
+	// noinspection JSMethodCanBeStatic
 	// eslint-disable-next-line no-empty-function
-	listener () {
-
+	listener (data) {
+		const strData = data.toString(); // eslint-disable-line no-unused-vars
 	}
 }
 
