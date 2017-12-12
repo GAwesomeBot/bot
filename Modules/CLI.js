013123d8f6f9f5cd0ce99eeea61bf9ec2985be79
TTtie <tttiett@outlook.com>
2017-12-12 18:57:58 +0100

    [eslint|CLI] Add the base listener + dispose function

--- Modules/CLI.js
+++ Modules/CLI.js
@@ -1,13 +1,22 @@
 class CLI {
-    constructor(db, client) {
-        this.db = db;
-        this.bot = this.client = client;
-        this.stdin = process.stdin;
-    }
+	constructor (db, client) {
+		this.db = db;
+		this.bot = this.client = client;
+		this.stdin = process.stdin;
+	}
 
-    setup() {
-        // TODO: this
-    }
+	setup () {
+		this.stdin.on("data", this.listener.bind(this));
+	}
+
+	dispose () {
+		this.stdin.removeListener("data", this.listener.bind(this));
+	}
+
+	// eslint-disable-next-line no-empty-function
+	listener () {
+
+	}
 }
 
 module.exports = CLI;
