b92e24e4d1d71ffe5430f9cfc81a0f125503bf07
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-19 11:23:27 +0200

    [Cache] Fix bugs that Gilbert found They're fixed! So thats good!

--- Internals/Events/ready/GAB.Cache.js
+++ Internals/Events/ready/GAB.Cache.js
@@ -2,19 +2,21 @@ const BaseEvent = require("../BaseEvent");
 
 class CacheHandler extends BaseEvent {
 	async handle () {
-		winston.debug(`Adding our server documents to the cache...`);
-		const serverDocuments = await Servers.find({
-			_id: {
-				$in: Array.from(this.bot.guilds.keys()),
-			},
-		}).exec();
-		if (serverDocuments) {
-			winston.debug(`Caching ${serverDocuments.length} servers`);
-			for (const serverDocument of serverDocuments) {
-				winston.silly(`Added "${this.bot.guilds.get(serverDocument._id)}" to the cache`, { guild: serverDocument._id });
-				this.bot.cache.set(serverDocument._id, serverDocument);
+		this.client.once("allDocumentsIn", async () => {
+			winston.debug(`Adding our server documents to the cache...`);
+			const serverDocuments = await Servers.find({
+				_id: {
+					$in: Array.from(this.bot.guilds.keys()),
+				},
+			}).exec();
+			if (serverDocuments.length) {
+				winston.debug(`Caching ${serverDocuments.length} servers`);
+				for (const serverDocument of serverDocuments) {
+					winston.silly(`Added "${this.bot.guilds.get(serverDocument._id)}" to the cache`, { guild: serverDocument._id });
+					this.bot.cache.set(serverDocument._id, serverDocument);
+				}
 			}
-		}
+		});
 	}
 }
 
