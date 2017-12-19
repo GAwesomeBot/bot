1a85bbad15ab5b3e0703aab698fa7b5c9b653d6c
Gilbert Gobbels <gijsh55@gmail.com>
2017-12-19 14:14:15 +0100

    [Commands] about command updates

--- Commands/Public/about.js
+++ Commands/Public/about.js
@@ -1,16 +1,10 @@
 module.exports = async ({ bot, configJS }, documents, msg, commandData) => {
 	const description = [
-		`Hello! I'm **${bot.user.username}**, a self-host of **GAwesomeBot**, the best Discord Bot! 🐬`,
-		`Created by GG142 and [the team](${configJS.hostingURL}#team)! ❤️`,
+		`Hello! I'm **${bot.user.username}**, running **GAwesomeBot ${configJSON.version} on branch ${configJSON.branch}**, the best Discord Bot! 🐬`,
+		`Created by Gilbert and [the GAwesomeDevs](${configJS.hostingURL}#team)! ❤`,
 		`Built using [Node.JS](https://nodejs.org/en/) and [Discord.js](https://discord.js.org/#/)`,
 	].join("\n");
-	const fields = [
-		{
-			name: `Version`,
-			value: `**${configJSON.version}** on branch\n**${configJSON.branch}**`,
-			inline: true,
-		},
-	];
+	const fields = [];
 	configJS.hostingURL && fields.push({
 		name: `Want to learn more?`,
 		value: `Click [here](${configJS.hostingURL})`,
@@ -24,7 +18,7 @@ module.exports = async ({ bot, configJS }, documents, msg, commandData) => {
 	msg.channel.send({
 		embed: {
 			color: 0x43B581,
-			fields,
+			fields: fields.length > 0 ? fields : undefined,
 			description,
 			footer: {
 				text: `Use "${msg.guild.commandPrefix}help" to list all commands that you can use in this server`,
