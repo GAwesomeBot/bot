8e9d3e5eb5e4997350f820306bef4aafb37b320a
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-21 21:46:31 +0200

    Woopsie daisy you did NOT see this

--- Database/Driver.js
+++ Database/Driver.js
@@ -24,7 +24,7 @@ exports.initialize = (url, client = null) => new Promise((resolve, reject) => {
 					let cachedDocument = global.ThatClientThatDoesCaching.cache.getSync(this._id);
 					if (this.__v && cachedDocument && cachedDocument.__v && this.__v !== cachedDocument.__v) {
 						winston.verbose(`The document for ${this._id} has a new version! Updating...`, { old: cachedDocument.__v, new: this.__v, guild: this._id });
-						global.ThatClientThatDoesCaching.update(this._id);
+						global.ThatClientThatDoesCaching.cache.update(this._id);
 					}
 				}
 			});
