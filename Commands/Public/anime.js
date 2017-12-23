f8be9983e5b545a1f2a5ac14f08aca04507a93b1
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-23 15:52:22 +0200

    [Commands/Utils] Reaction Menu and anime commands are done!

--- Commands/Public/anime.js
+++ Commands/Public/anime.js
@@ -17,8 +17,62 @@ module.exports = async ({ Constants: { Colors } }, { serverDocument }, msg, comm
 		let { body, status } = await get(API).set("Accept", "application/vnd.api+json");
 		body = JSON.parse(body.toString());
 		if (status === 200 && body.data && body.data.length) {
-			let m = await msg.channel.send(`test`);
-			let menu = new ReactionMenu(m, msg, ["i", "like", "eggs"]);
+			const list = [];
+			const results = [];
+			const getDisplay = data => {
+				// Title + airing time
+				let airing = data.attributes.startDate;
+				if (data.attributes.endDate !== null) {
+					airing += ` — ${data.attributes.endDate}`;
+				}
+				let totalResult = "";
+				if (data.attributes.averageRating) {
+					totalResult += `Rating is ${data.attributes.averageRating}%`;
+				}
+				if (data.attributes.ageRating && data.attributes.averageRating) {
+					totalResult += ` | Rated ${data.attributes.ageRating}`;
+				} else {
+					totalResult += `Rated ${data.attributes.ageRating}`;
+				}
+				if (!data.attributes.episodeCount) {
+					data.attributes.episodeCount = "N/A";
+				}
+				if (data.attributes.episodeLength) {
+					totalResult += ` | ${data.attributes.episodeCount} episode${data.attributes.episodeCount === 1 ? "" : "s"}, each lasting ${data.attributes.episodeLength} minutes.`;
+				} else {
+					totalResult += ` | ${data.attributes.episodeCount} episode${data.attributes.episodeCount === 1 ? "" : "s"}.`;
+				}
+				return {
+					embed: {
+						color: 0x00FF00,
+						author: {
+							iconURl: `${data.attributes.posterImage.tiny}`,
+						},
+						title: `${data.attributes.canonicalTitle} (${airing})`,
+						url: `https://kitsu.io/anime/${data.attributes.slug}`,
+						description: `${data.attributes.synopsis.split("").splice(0, 1500).join("")}${data.attributes.synopsis.length > 1500 ? `...\nRead more [here](https://kitsu.io/anime/${data.attributes.slug})` : ""}`,
+						footer: {
+							text: `${totalResult}\nClick on the "Anime Name" to see the webpage!`,
+						},
+						image: {
+							url: `${data.attributes.posterImage.original}`,
+						},
+					},
+				};
+			};
+			for (let i = 0; i < number; i++) {
+				const entry = body.data[i];
+				if (entry) {
+					results.push(getDisplay(entry));
+					list.push(`[ ${i + 1} ] ${entry.attributes.canonicalTitle}`);
+				}
+			}
+			if (list.length === 1) {
+				msg.channel.send(results[0]);
+			} else {
+				// eslint-disable-next-line no-new
+				new ReactionMenu(msg, list, results);
+			}
 		} else {
 			winston.verbose(`Couldn't find any animes for "${query}"`, { svrid: msg.guild.id, chid: msg.channel.id, usrid: msg.author.id });
 			msg.channel.send({
--- Modules/MessageUtils/ReactionBasedMenu.js
+++ Modules/MessageUtils/ReactionBasedMenu.js
@@ -1,8 +1,10 @@
+const { Colors } = require("../../Internals/Constants");
+
 class ReactionBasedMenu {
-	constructor (msg, originalMsg, options, results, embed = null) {
+	constructor (msg, options, results, embed = null) {
 		this.client = msg.client;
-		this.msg = msg;
-		this.originalMsg = originalMsg;
+		this.originalMsg = msg;
+
 		this.emojis = {
 			back: "◀",
 			forward: "▶",
@@ -29,18 +31,27 @@ class ReactionBasedMenu {
 		/** Current page is 1-based, code-wise is 0-based */
 		this.currentPage = 0;
 		this.totalPages = 0;
+		this.shouldAdd1 = true;
+
+		this.choice = null;
 
 		this.embed = embed ? embed : {
 			title: `Choose a number`,
-			color: 0x3669FA,
+			color: Colors.BLUE,
 			description: `{list}`,
-			footer: `Page {current page} / {total pages}`,
+			footer: `Page {current page} out of {total pages}`,
 		};
 
 		this.initCollector();
 	}
 
 	async initCollector () {
+		if (this.options.length > 10) {
+			this.options = this.options.chunk(10);
+			this.totalPages = this.options.length;
+			this.shouldAdd1 = false;
+		}
+		await this.sendInitialMessage();
 		await this.prepareReactions();
 		this.collector = this.msg.createReactionCollector(
 			(reaction, user) => user.id === this.originalMsg.author.id && this.emojiArray.includes(reaction.emoji.name),
@@ -49,11 +60,26 @@ class ReactionBasedMenu {
 		this.handle();
 	}
 
+	async sendInitialMessage () {
+		this.msg = await this.originalMsg.channel.send({
+			embed: {
+				color: this.embed.color,
+				title: this.embed.title,
+				description: this.embed.description.format({ list: `\`\`\`ini\n${this._menuOptions}\`\`\`` }),
+				footer: {
+					text: this.embed.footer.format({ "current page": this.currentPage + 1, "total pages": this.shouldAdd1 ? this.totalPages + 1 : this.totalPages }),
+				},
+			},
+		});
+	}
+
+	get _menuOptions () {
+		return this.totalPages > 0 ? this.options[this.currentPage].map(i => i).join("\n") : this.options.map(i => i).join("\n");
+	}
+
 	async prepareReactions () {
-		if (this.options.length > 10) {
+		if (this.totalPages > 0) {
 			await this._allOptions();
-			this.options = this.options.chunk(10);
-			this.totalPages = this.options.length;
 		} else {
 			await this._options(this.options.length);
 		}
@@ -61,20 +87,19 @@ class ReactionBasedMenu {
 
 	async _allOptions () {
 		await this.msg.react(this.emojis.back);
-		for (const emoji of this.numberEmojis) await this.msg.react(emoji);
+		for (const emoji of Object.values(this.numberEmojis)) await this.msg.react(emoji);
 		await this.msg.react(this.emojis.forward);
 		await this.msg.react(this.emojis.stop);
 	}
 
 	async _options (number) {
-		await this.msg.react(this.emojis.back);
 		for (const emoji of Object.values(this.numberEmojis).splice(0, number)) await this.msg.react(emoji);
 		await this.msg.react(this.emojis.stop);
 	}
 
 	async handle () {
 		this.collector.on("collect", async reaction => {
-			if (reaction.emoji.name === this.emojis.stop) return this._handleStop();
+			if (reaction.emoji.name === this.emojis.stop) return this.collector.stop();
 			if (Object.values(this.numberEmojis).includes(reaction.emoji.name)) return this._handleNumberInput(reaction);
 			if (["◀", "▶"].includes(reaction.emoji.name)) return this._handlePageChange(reaction);
 		});
@@ -83,22 +108,114 @@ class ReactionBasedMenu {
 	}
 
 	async _handleNumberInput (reaction) {
-		console.log(`Got number!`);
-
+		this.collector.stop("manual");
+		switch (reaction.emoji.name) {
+			case "1⃣": {
+				this._updateChoice(0);
+				break;
+			}
+			case "2⃣": {
+				this._updateChoice(1);
+				break;
+			}
+			case "3⃣": {
+				this._updateChoice(2);
+				break;
+			}
+			case "4⃣": {
+				this._updateChoice(3);
+				break;
+			}
+			case "5⃣": {
+				this._updateChoice(4);
+				break;
+			}
+			case "6⃣": {
+				this._updateChoice(5);
+				break;
+			}
+			case "7⃣": {
+				this._updateChoice(6);
+				break;
+			}
+			case "8⃣": {
+				this._updateChoice(7);
+				break;
+			}
+			case "9⃣": {
+				this._updateChoice(8);
+				break;
+			}
+			case "🔟": {
+				this._updateChoice(9);
+				break;
+			}
+		}
 	}
 
-	async _handleStop () {
-		try {
-			await this.msg.clearReactions();
-		} catch (err) {
-			winston.verbose(`Failed to clear reactions from message for reaction menu..`, { msg: this.msg.id, chid: this.msg.channel.id, svrid: this.msg.guild.id }, err);
+	async _handleStop (_, reason) {
+		if (reason === "manual") {
+			try {
+				await this.msg.clearReactions();
+			} catch (err) {
+				winston.verbose(`Failed to clear all reactions for interactive menu, will remove only the bots reaction!`, { err: err.name });
+				this.msg.reactions.forEach(r => r.remove());
+			}
+		} else {
+			this.msg.delete();
+			this.originalMsg.channel.send({
+				embed: {
+					color: Colors.LIGHT_ORANGE,
+					description: `You've exited the menu or the menu expired.`,
+				},
+			});
 		}
 		// Null out the collector
 		this.collector = null;
 	}
 
 	async _handlePageChange (reaction) {
-		console.log(`Got page change!`);
+		switch (reaction.emoji.name) {
+			case "◀": {
+				this.currentPage--;
+				if (this.currentPage <= 0) this.currentPage = 0;
+				this.removeUserReaction(reaction, this.originalMsg.author.id);
+				this._update();
+				break;
+			}
+			case "▶": {
+				this.currentPage++;
+				if (this.currentPage >= this.totalPages) this.currentPage = this.totalPages - 1;
+				this.removeUserReaction(reaction, this.originalMsg.author.id);
+				this._update();
+				break;
+			}
+		}
+	}
+
+	async _update () {
+		this.msg = await this.msg.edit({
+			embed: {
+				color: this.embed.color,
+				title: this.embed.title,
+				description: this.embed.description.format({ list: `\`\`\`ini\n${this._menuOptions}\`\`\`` }),
+				footer: {
+					text: this.embed.footer.format({ "current page": this.currentPage + 1, "total pages": this.shouldAdd1 ? this.totalPages + 1 : this.totalPages }),
+				},
+			},
+		});
+	}
+
+	async removeUserReaction (reaction, user) {
+		try {
+			await reaction.remove(user);
+		} catch (err) {
+			winston.verbose(`Failed to remove the reaction for user!`, { user, message: reaction.message.id });
+		}
+	}
+
+	async _updateChoice (number) {
+		this.msg.edit(this.results[number + (this.currentPage * 10)]);
 	}
 }
 
