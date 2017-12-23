c59b0e0f447cb142d4810952ace69605ec5643ad
TTtie <tttiett@outlook.com>
2017-12-23 09:14:06 +0100

    [CLI/Commands] Use GAB's natural shutdown and restart

--- Commands/CLI/restart.js
+++ Commands/CLI/restart.js
@@ -1,12 +1,4 @@
 const { spawn } = require("child_process");
 module.exports = async ({ cli }) => {
-	winston.info("Be right back!");
-	cli.dispose();
-	const proc = spawn("node", ["bot"], {
-		detached: true,
-		shell: true,
-	});
-	proc.unref();
-	cli.sharder.IPC.send("shutdown", {});
-	process.exit(0);
+	cli.sharder.IPC.onEvents.get("shutdown")({ err: false, soft: true });
 };
--- Commands/CLI/shutdown.js
+++ Commands/CLI/shutdown.js
@@ -1,6 +1,4 @@
 module.exports = async ({ cli }) => {
 	winston.info("See you soon!");
-	cli.dispose();
-	cli.sharder.IPC.send("shutdown", {});
-	process.exit(0);
+	cli.sharder.IPC.onEvents.get("shutdown")({ err: false, soft: false });
 };
