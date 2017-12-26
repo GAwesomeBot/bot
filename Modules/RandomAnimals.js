da605419d164d7e0223d35587e84043a3c09f331
Frangu Vlad <contact@kingdgrizzle.ml>
2017-12-26 16:37:19 +0200

    [Modules] Add random dogs in the mix!

--- Modules/RandomAnimals.js
+++ Modules/RandomAnimals.js
@@ -2,7 +2,7 @@ const { Error } = require("../Internals/Errors/");
 const { get } = require("snekfetch");
 
 const CAT = `https://random.cat/meow`;
-const DOG = `https://random.dom/woof`;
+const DOG = `https://random.dog/woof`;
 
 module.exports = class RandomAnimals {
 	constructor () {
@@ -17,5 +17,19 @@ module.exports = class RandomAnimals {
 			throw err;
 		}
 		return res.body.file;
+	}
+
+	static async dog () {
+		let res;
+		try {
+			res = await get(DOG);
+		} catch (err) {
+			throw err;
+		}
+		if (res.text.includes(".mp4")) {
+			return RandomAnimals.dog();
+		} else {
+			return `https://random.dog/${res.text}`;
+		}
 	}
 };
