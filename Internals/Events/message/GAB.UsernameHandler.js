c622b2d48d78e63b84d1190e1cb2aed57c90de28
Frangu Vlad <contact@kingdgrizzle.ml>
2018-01-06 18:12:27 +0200

    [Events] We don't log boats. They are equal. We jsut don't log them!

--- Internals/Events/message/GAB.UsernameHandler.js
+++ Internals/Events/message/GAB.UsernameHandler.js
@@ -4,6 +4,10 @@ const BaseEvent = require("../BaseEvent");
  * Yes, this is a thing
  */
 class UsernameHandler extends BaseEvent {
+	requirements (msg) {
+		return !msg.author.bot;
+	}
+
 	async prerequisite (msg) {
 		this.userDocument = (await Users.findOrCreate({ _id: msg.author.id })).doc;
 	}
