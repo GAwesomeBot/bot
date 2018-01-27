5470ce97b1a0a06cd80f412bd2993ea5629f3691
Frangu Vlad <contact@kingdgrizzle.ml>
2018-01-27 13:54:02 +0200

    :bug: Allow edits to occur in DM channels too

--- Internals/Events/messageUpdate/GAB.MessageCommandUpdateHandler.js
+++ Internals/Events/messageUpdate/GAB.MessageCommandUpdateHandler.js
@@ -2,7 +2,6 @@ const BaseEvent = require("../BaseEvent");
 
 module.exports = class extends BaseEvent {
 	requirements (old, msg) {
-		if (!msg.guild) return false;
 		if (old.content !== msg.content) return true;
 	}
 
