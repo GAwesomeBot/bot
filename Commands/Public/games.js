90e55d225d708e83b91971d988c632dc5af0069a
wasdennnoch <leiter04@gmail.com>
2018-03-17 19:32:38 +0100

    Improve games command, add total game count and playtime

--- Commands/Public/games.js
+++ Commands/Public/games.js
@@ -1,21 +1,31 @@
 const moment = require("moment");
 
 module.exports = async ({ Constants: { Colors } }, { serverDocument }, msg, commandData) => {
-	const description = serverDocument.games.sort((a, b) => b.time_played - a.time_played)
+	const sortedGames = serverDocument.games.sort((a, b) => b.time_played - a.time_played);
+	let totalTime = 0;
+	let totalGames = sortedGames.length;
+	sortedGames.forEach(game => {
+		totalTime += game.time_played * 5;
+	});
+	const description = sortedGames
 		.slice(0, 8)
 		.map(game => {
-			let timePlayed = game.time_played * 5;
+			const timePlayed = game.time_played * 5;
+			const timeStringSplit = moment.duration(timePlayed, "minutes").humanize().split(" ");
 			return [
 				`» **${game._id}** «`,
-				`\tPlayed for **${moment.duration(timePlayed, "minutes").humanize()}** total this week.`,
+				`\t**${timeStringSplit[0]}** ${timeStringSplit[1]}`,
 			].join("\n");
 		});
 	if (description.length) {
 		msg.send({
 			embed: {
 				color: Colors.SUCCESS,
-				title: `Here ${description.length === 1 ? "is" : "are"} the top ${description.length} game${description.length === 1 ? "" : "s"} played in this server!`,
+				title: `Here ${description.length === 1 ? "is" : "are"} the ${description.length === 1 ? "only game" : `top ${description.length} games`} played in this server this week!`,
 				description: description.join("\n\n"),
+				footer: {
+					text: `There were ${totalGames} games played this week with a combined playtime of ${moment.duration(totalTime, "minutes").humanize()}!`,
+				},
 			},
 		});
 	} else {
