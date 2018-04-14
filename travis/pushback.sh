c0820057730d6be39e97b4910b3264b6b71edaba
Gilbert Gobbels <gijsh55@gmail.com>
2018-04-14 17:49:42 +0200

    :green_heart:

--- travis/pushback.sh
+++ travis/pushback.sh
@@ -3,10 +3,7 @@
 git config --global user.email "gawesomebot@kingdgrizzle.ml"
 git config --global user.name "GAwesomeBot"
 
-git checkout -b $TRAVIS_BRANCH
 git add -A
 git commit --message ":rocket: Build: $TRAVIS_BUILD_NUMBER"
 
-git remote add origin-target https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null 2>&1
-git branch -u origin-target/indev-4.0.2
-git push origin-target $TRAVIS_BRANCH
+git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} $TRAVIS_BRANCH
