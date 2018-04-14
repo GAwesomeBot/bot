feb137e0036c4058250a14648ff2d2ad7b53a04d
Gilbert Gobbels <gijsh55@gmail.com>
2018-04-14 17:57:45 +0200

    :green_heart:

--- travis/pushback.sh
+++ travis/pushback.sh
@@ -3,6 +3,7 @@
 git config --global user.email "gawesomebot@kingdgrizzle.ml"
 git config --global user.name "GAwesomeBot"
 
+git checkout indev-4.0.2
 git add -A
 git commit --message ":rocket: Build: $TRAVIS_BUILD_NUMBER"
 
