18c3320690392332691353d57123317830cb5149
Gilbert Gobbels <gijsh55@gmail.com>
2018-04-15 15:22:32 +0200

    :speech_balloon: Change build commit message

--- travis/pushback.sh
+++ travis/pushback.sh
@@ -5,6 +5,6 @@ git config --global user.name "GAwesomeBot"
 
 git checkout indev-4.0.2
 git add -A
-git commit --message ":rocket: Build: $TRAVIS_BUILD_NUMBER"
+git commit -m ":rocket: :package: Minify source files" -m "Build: $TRAVIS_COMMIT ($TRAVIS_BUILD_NUMBER)"
 
 git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} $TRAVIS_BRANCH
