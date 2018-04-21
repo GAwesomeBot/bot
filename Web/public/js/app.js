c11c584a8a0cb77b46fb00083b8678899057697b
Gilbert Gobbels <gijsh55@gmail.com>
2018-04-21 17:54:21 +0200

    :art: Show "Nothing to see here" box if last element is removed

--- Web/public/js/app.js
+++ Web/public/js/app.js
@@ -628,11 +628,15 @@ GAwesomeUtil.dashboard.removeElement = elem => {
 		button.addClass("is-loading");
 
 		const tableRow = button.parents("tr");
-		const isLastTableRow = tableRow.is(":last-child")
+		const isLastTableRow = tableRow.is(":only-child")
 		const buttonName = button.attr("name");
 
 		const afterDelete = () => {
 			tableRow.fadeOut(400, () => tableRow.remove());
+			if (isLastTableRow) {
+				tableRow.parents("table").addClass("is-hidden");
+				$(".no-elements-message").removeClass("is-hidden");
+			}
 		};
 
 		$.ajax({
