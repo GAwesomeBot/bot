a6cd8ce816e041b664bcd61431c818ec23bc203b
MrWasdennnoch <leiter04@gmail.com>
2018-04-22 11:44:00 +0200

    Add urban command (#252)
    :sparkles: Add urban command

--- Internals/Constants.js
+++ Internals/Constants.js
@@ -215,9 +215,10 @@ exports.APIs = {
 	CATFACT: number => `https://catfact.ninja/facts?limit=${number}`,
 	DOGFACT: number => `https://dog-api.kinduff.com/api/facts?number=${number}`,
 	E621: query => `https://e621.net/post/index.json?tags=${encodeURIComponent(query)}&limit=256`,
-	SPOOPYLINK: url => `https://spoopy.link/api/${url}`,
 	FORTUNE: (category = null) => `http://yerkee.com/api/fortune/${category ? category : ""}`,
 	GIPHY: (token, query, nsfw) => `http://api.giphy.com/v1/gifs/random?api_key=${token}&rating=${nsfw}&format=json&limit=1&tag=${encodeURIComponent(query)}`,
+	SPOOPYLINK: url => `https://spoopy.link/api/${url}`,
+	URBAN: (term, page = 1) => `https://api.urbandictionary.com/v0/${!term ? "random" : `define?page=${page}&term=${encodeURIComponent(term)}`}`,
 };
 
 exports.EmptySpace = `\u200b`;
