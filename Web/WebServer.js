ecc5352b7edf1b218f2f87966aa7ad1289ba6f8c
Gilbert Gobbels <gijsh55@gmail.com>
2018-11-26 15:24:57 +0100

    :bug: Make static router bit less shit. Only a bit.

--- Web/WebServer.js
+++ Web/WebServer.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const ejs = require("ejs");
 const session = require("express-session");
-const mongooseSessionStore = require("connect-mongo")(session);
+const mongoSessionStore = require("connect-mongo")(session);
 const passport = require("passport");
 const passportSocketIo = require("passport.socketio");
 const discordStrategy = require("passport-discord").Strategy;
@@ -119,8 +119,8 @@ exports.open = async (client, auth, configJS, winston) => {
 		done(null, id);
 	});
 
-	const sessionStore = new mongooseSessionStore({
-		mongooseConnection: Database.Raw,
+	const sessionStore = new mongoSessionStore({
+		db: EDatabase.client,
 	});
 
 	app.use(session({
@@ -138,11 +138,13 @@ exports.open = async (client, auth, configJS, winston) => {
 	app.use(middleware.logRequest);
 
 	// (Horribly) serve public dir
-	app.use("/static/:type", (req, res, next) => {
-		if (req.get("Accept") && req.get("Accept").includes("image/webp") && req.params.type === "img" && ![".gif", "webp"].includes(req.path.substr(req.path.length - 4))) {
-			res.redirect(`/static/img${req.path.substring(0, req.path.lastIndexOf("."))}.webp`);
+	const staticRouter = express.static(`${__dirname}/public/`, { maxAge: 86400000 });
+	app.use("/static", (req, res, next) => {
+		const fileExtension = req.path.substr(req.path.lastIndexOf("."));
+		if (req.get("Accept") && req.get("Accept").includes("image/webp") && req.path.startsWith("/img") && ![".gif", ".webp"].includes(fileExtension)) {
+			res.redirect(`/static${req.path.substring(0, req.path.lastIndexOf("."))}.webp`);
 		} else {
-			return express.static(`${__dirname}/public/${req.params.type}`, { maxAge: 86400000 })(req, res, next);
+			return staticRouter(req, res, next);
 		}
 	});
 
