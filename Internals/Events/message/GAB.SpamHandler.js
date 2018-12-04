83124d56640c7605e5585d432ce36ad6dc0e8bb8
Gilbert Gobbels <gijsh55@gmail.com>
2018-12-04 19:00:24 +0100

    :bug: Fix bug

--- Internals/Events/message/GAB.SpamHandler.js
+++ Internals/Events/message/GAB.SpamHandler.js
@@ -140,7 +140,7 @@ class SpamHandler extends BaseEvent {
 								violatorRoleID = this.serverDocument.config.moderation.filters.spam_filter.violator_role_id;
 							}
 							// eslint-disable-next-line max-len
-							this.client.handleViolation(msg.guild, this.serverDocument, msg.channel, msg.member, userDocument, memberDocument, `You continued to spam in #${msg.channel.name} (${msg.channel}) on ${msg.guild}`, `**@${this.bot.getName(this.serverDocument, msg.member, true)}** (${msg.member}) continues to spam in #${msg.channel.name} (${msg.channel}) on ${msg.guild}`, `Second-time spam violation in #${msg.channel.name} (${msg.channel})`, this.serverDocument.config.moderation.filters.spam_filter.action, violatorRoleID);
+							this.client.handleViolation(msg.guild, this.serverDocument, msg.channel, msg.member, userDocument, memberDocument, `You continued to spam in #${msg.channel.name} (${msg.channel}) on ${msg.guild}`, `**@${this.client.getName(this.serverDocument, msg.member, true)}** (${msg.member}) continues to spam in #${msg.channel.name} (${msg.channel}) on ${msg.guild}`, `Second-time spam violation in #${msg.channel.name} (${msg.channel})`, this.serverDocument.config.moderation.filters.spam_filter.action, violatorRoleID);
 						}
 						await userDocument.save().catch(err => {
 							winston.debug(`Failed to save user document...`, err);
