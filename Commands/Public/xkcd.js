ea4b5c24ac76b2bdde8820ec26212a2288273ff9
Gilbert Gobbels <gijsh55@gmail.com>
2018-12-17 22:28:38 +0100

    :ok_hand: Xkcd command requests
    * :speech_balloon: Fix typos
    * :art: Remove code dupe

--- Commands/Public/xkcd.js
+++ Commands/Public/xkcd.js
@@ -1,14 +1,14 @@
 const fetch = require("chainfetch");
 
 module.exports = async ({ Constants: { Colors, Text, APIs, UserAgent } }, documents, msg) => {
+	const comicData = await fetch.get(APIs.XKCD(msg.suffix ? msg.suffix : undefined)).set({ "User-Agent": UserAgent, Accept: "application/json" }).onlyBody()
+		.catch(() => null);
 	if (msg.suffix) {
-		const comicData = await fetch.get(APIs.XKCD(msg.suffix)).set({ "User-Agent": UserAgent, Accepts: "application/json" }).onlyBody()
-			.catch(() => null);
 		if (!comicData) {
 			msg.send({
 				embed: {
 					color: Colors.SOFT_ERR,
-					description: `Doh! An XKCD comic with ID ${msg.suffix} wasn't found.`,
+					description: `Doh! A XKCD comic with ID ${msg.suffix} wasn't found.`,
 				},
 			});
 		} else {
@@ -26,30 +26,26 @@ module.exports = async ({ Constants: { Colors, Text, APIs, UserAgent } }, docume
 				},
 			});
 		}
+	} else if (!comicData) {
+		msg.send({
+			embed: {
+				color: Colors.SOFT_ERR,
+				description: `Doh! The current XKCD comic couldn't be fetched.`,
+			},
+		});
 	} else {
-		const comicData = await fetch.get(APIs.XKCD()).set({ "User-Agent": UserAgent, Accepts: "application/json" }).onlyBody()
-			.catch(() => null);
-		if (!comicData) {
-			msg.send({
-				embed: {
-					color: Colors.SOFT_ERR,
-					description: `Doh! The current XKCD comic couldn't be fetched.`,
+		msg.send({
+			embed: {
+				color: Colors.RESPONSE,
+				title: comicData.title,
+				image: {
+					url: comicData.img || comicData.alt,
 				},
-			});
-		} else {
-			msg.send({
-				embed: {
-					color: Colors.RESPONSE,
-					title: comicData.title,
-					image: {
-						url: comicData.img || comicData.alt,
-					},
-					footer: {
-						text: `Latest XKCD Comic | #${comicData.num}`,
-					},
-					timestamp: new Date(`${comicData.year}-${comicData.month}-${comicData.day}`),
+				footer: {
+					text: `Latest XKCD Comic | #${comicData.num}`,
 				},
-			});
-		}
+				timestamp: new Date(`${comicData.year}-${comicData.month}-${comicData.day}`),
+			},
+		});
 	}
 };
