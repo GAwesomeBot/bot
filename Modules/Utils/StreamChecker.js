863c2244a74a4a75dec22da2aed6df7e281617e6
Gilbert Gobbels <gijsh55@gmail.com>
2019-04-25 11:56:24 +0200

    :sparkles: Streamers command + eval command enhances (#440)
    * :sparkles: Streamers command + eval command enhances
    
    * :sparkles: Port streamers command over from V4
    * :sparkles: If the eval command is executed with the --unsafe argument, tokens will be accessible by the eval function. Tokens are always filtered in the final message sent to Discord, this argument does not affect that behavior.
    * :bug: Do not send streamer alerts if no channel is configured
    
    * OWO
    
    Co-Authored-By: GilbertGobbels <gijsh55@gmail.com>
    
    * UWU
    
    Co-Authored-By: GilbertGobbels <gijsh55@gmail.com>

--- Modules/Utils/StreamChecker.js
+++ Modules/Utils/StreamChecker.js
@@ -12,7 +12,7 @@ module.exports = async (client, server, serverDocument, streamerDocument) => {
 			winston.verbose(`Streamer "${streamerDocument._id}" started streaming`, { svrid: server.id });
 			streamerQueryDocument.set("live_state", true);
 
-			const channel = streamerDocument.channel_id ? server.channels.get(streamerDocument.channel_id) : server.defaultChannel;
+			const channel = streamerDocument.channel_id ? server.channels.get(streamerDocument.channel_id) : null;
 			if (channel) {
 				const channelDocument = serverDocument.channels[channel.id];
 				if (!channelDocument || channelDocument.bot_enabled) {
